7e426c51bb7e15fdb210c3cae4bb7029cdb757fb
Ebrahim Byagowi <ebrahim@gnu.org>
2023-07-06 14:58:03 +0330

    Use service instead of the deprecated plist_options

--- Formula/persian-calendar.rb
+++ Formula/persian-calendar.rb
@@ -6,30 +6,12 @@ class PersianCalendar < Formula
   sha256 "157c34fb1f52b9bf310a8dd75008c4ca6a57bc460ba613093f6d7f88796a2fd4"
 
   def install
-    cp "persiancalendar.swift", "#{prefix}"
+    cp "persiancalendar.swift", "#{opt_prefix}"
   end
 
-  plist_options :startup => true
-
-  def plist; <<-EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>/usr/bin/swift</string>
-          <string>#{prefix}/persiancalendar.swift</string>
-        </array>
-        <key>KeepAlive</key>
-        <false/>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-    </plist>
-    EOS
+  service do
+    run ["/usr/bin/swift", "#{opt_prefix}/persiancalendar.swift"]
+    require_root false
   end
 
   test do
