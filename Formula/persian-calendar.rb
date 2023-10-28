ef3940abb2aa91a73f297fdc5edc7073c7142864
Ebrahim Byagowi <ebrahim@gnu.org>
2023-10-28 20:35:20 +0330

    Use compiled version of persiancalendar binary
    As:
    
    $ swift /opt/homebrew/opt/persian-calendar/persiancalendar.swift
    JIT session error: Symbols not found: [ _OBJC_CLASS_$_NSMenu, _NSApp, _OBJC_CLASS_$_NSApplication, _OBJC_CLASS_$_NSPasteboard, _NSPasteboardTypeString,
    ...

--- Formula/persian-calendar.rb
+++ Formula/persian-calendar.rb
@@ -6,11 +6,11 @@ class PersianCalendar < Formula
   sha256 "157c34fb1f52b9bf310a8dd75008c4ca6a57bc460ba613093f6d7f88796a2fd4"
 
   def install
-    cp "persiancalendar.swift", "#{opt_prefix}"
+    exec "/usr/bin/swiftc", "persiancalendar.swift", "-o", "#{opt_prefix}/persiancalendar"
   end
 
   service do
-    run ["/usr/bin/swift", "#{opt_prefix}/persiancalendar.swift"]
+    run ["#{opt_prefix}/persiancalendar"]
     require_root false
   end
 
